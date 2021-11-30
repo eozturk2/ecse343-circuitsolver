6051133de718ab6be236470c930892b45da3f1ad
Edem Nuviadenu <edemnuviadenu@gmail.com>
2021-11-30 14:29:55 -0500

    change part1 to matlab code file

---
+++ Part1.m
@@ -0,0 +1,24 @@
+
+% Matlab program that computes the solution for the output voltage V2
+fun = @nonlinearFunc
+n = 4
+xstart = zeros(n,1)
+% per the documentation found here:
+% https://www.mathworks.com/help/optim/ug/nonlinear-equations-with-jacobian.html
+% matlab provides an fsolve method to solve nonlinear equations. Using the 
+% default options to solve the system for x, results in higher number of
+% iterations. To optimize this, we decide to set the options of the solver to 
+% include the Jacobian of the matrix via the 'SpecifyObjectiveGradient' parameter
+
+options = optimoptions('fsolve','SpecifyObjectiveGradient',true);
+[x,fval,exitflag,output] = fsolve(fun,xstart,options);
+
+x
+% display the quality of the solution
+quality = norm(fval) 
+% number of function evaluations taken
+disp(output.funcCount)
+
+V2 = x(2)
+
+% TODO: verify with spice
